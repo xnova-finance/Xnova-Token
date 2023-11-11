b8a3a9154790f468dab5191cddf54c270ad714dc
xnova-finance <team@xnova.io>
2023-11-11 20:15:13 +0100

    init

---
+++ deploy/00_deploy_xnova.ts
@@ -0,0 +1,18 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  await deploy("XNOVA", {
+    from: deployer,
+    log: true,
+
+    autoMine: true,
+  });
+};
+
+export default deployYourContract;
+
+deployYourContract.tags = ["xnova"];
---
+++ test/xnova.ts
@@ -0,0 +1,15 @@
+// import { expect } from "chai";
+import { ethers } from "hardhat";
+import { XNOVA } from "../typechain-types";
+
+describe("XNOVA", function () {
+  // We define a fixture to reuse the same setup in every test.
+
+  let xnova: XNOVA;
+  before(async () => {
+    const [owner] = await ethers.getSigners();
+    const xnovaFactory = await ethers.getContractFactory("YourContract");
+    xnova = (await xnovaFactory.deploy(owner.address)) as XNOVA;
+    await xnova.deployed();
+  });
+});
